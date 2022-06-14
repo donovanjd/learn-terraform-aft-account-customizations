e5e4df915fd622b041d17ee3a4132a2c4a99a662
donovanjd <donovandurand@gmail.com>
2022-06-14 22:01:50 +1000

    update s3 bucket name

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,11 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
-}
-
-resource "aws_s3_bucket" "sandbox_bucket-1" {
-  bucket = "aft-sandbox-1-${data.aws_caller_identity.current.account_id}"
+  bucket = "aft-sandbox1-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
